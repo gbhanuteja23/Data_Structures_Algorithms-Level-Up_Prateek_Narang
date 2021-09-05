635c7539c7381e2e93abd3c4243561709037af0b
G Bhanuteja <45899803+gbhanuteja23@users.noreply.github.com>
2021-09-05 19:49:35 +0530

    Triplets Problem

---
+++ 3-Course Notes/1- Arrays_Vectors/20- Triplets Code.cpp
@@ -0,0 +1,16 @@
+//Program Code for Triplets problem
+
+#include<iostream>
+#include<unordered_set>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+
+vector<vector<int> > triplets(vector<int>arr, int sum)
+{
+    //First of Sort the given Array
+    arr.sort(arr.begin(), arr.end());
+
+}
